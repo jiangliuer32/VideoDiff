03898934386d42e27bcd6a8367096b0c95ce06d6
a295290226a <74123335+a295290226a@users.noreply.github.com>
2023-07-02 23:05:51 +0800

    完善info类

--- VideoDiff.py
+++ VideoDiff.py
@@ -1,54 +1,9 @@
-from airtest.aircv.cal_confidence import *
-import os
-def makeFolderResult(imgPath, logName):
-    logFloder = os.path.join(imgPath, f'图片对比结果')
-    os.mkdir(logFloder)
-    logPath = os.path.join(imgPath, f'图片对比结果/{logName}')
-    return logPath
+import VideoInfo
 
-def wirteLog(msg, logPath):
-    with open(logPath, "a+", encoding='utf-8') as f:
-        f.write(msg)
-        f.write("\n")
+class VideoDiff(VideoInfo):
+    def int__(self,inFile,outFile,duration):
+        super().__init__(inFile, outFile)
+        self.duration = duration
 
-def imageCompare(imagePath, logPath,threshold:int):
-    '''
-    :param imagePath: 图片存放的路径
-    :param logPath: 日志存放的路径
-    :param threshold: 阈值,指定int类型
-    :return:
-    '''
-    needCompareImgDict = {}
-    for root, dirs, files in os.walk(imagePath):
-        for file in files:
-            if "_" in file:
-                key = str(file).split("_")[0]
-                if key not in needCompareImgDict.keys():
-                    needCompareImgDict[key] = [os.path.join(root, file)]
-                else:
-                    tempList = needCompareImgDict[key]
-                    tempList.append(os.path.join(root, file))
-                    needCompareImgDict[key] = tempList
-    #### 遍历字典,将同个ID下的图片进行对比
-    for imgs in needCompareImgDict.values():
-        for i in range(len(imgs) - 1):
-            img_1_path = imgs[i]
-            img_2_path = imgs[i + 1]
-            img_1_Name = img_1_path.split("\\")[-1]
-            img_2_Name = img_2_path.split("\\")[-1]
-            img1 = cv2.resize(cv2.imread(img_1_path), (370, 800)) # 图片尺寸根据实际图片写入
-            img2 = cv2.resize(cv2.imread(img_2_path), (370, 800))
-            confidence = cal_ccoeff_confidence(img1, img2)
-            if confidence > threshold:
-                writeMsg = f"【对比失败】,疑似 {img_1_Name}  与  {img_2_Name} 两张图片一致,相似度为：{round(confidence * 100, 2)}%"
-                print('1')
-                print(writeMsg)
-            else:
-                print('失败')
-                pass
+    def videoDiff(self,videoA,videoB):
 
-if __name__ == '__main__':
-    imagePath = "C:\\Users\\xiaoli\\Desktop\\ima"
-    logName = str(imagePath.split("\\")[-1]) + ".txt"
-    logPath = makeFolderResult(imagePath, logName)
-    imageCompare(imagePath, logPath,0.8)
--- VideoInfo.py
+++ VideoInfo.py
@@ -3,45 +3,42 @@ import numpy
 import cv2
 import sys
 import random
-import math
+
+class VideoInfo:
+    def __int__(self,inFile,outFile):
+        self.inFile = inFile
+        self.outFile = outFile
 
 
-
-def read_frame_by_time(in_file, time):
-    """
-    指定时间节点读取任意帧
-    """
-    out, err = (
-        ffmpeg.input(in_file, ss=time)
-              .output('pipe:', vframes=1, format='image2', vcodec='mjpeg')
-              .run(capture_stdout=True)
-    )
-    # image_array = numpy.asarray(bytearray(out), dtype="uint8")
-    # image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-    # cv2.imshow('frame', image)
-    # cv2.inweite('C:/Users/xiaoli/Desktop/VID20230702033914.mp4','test')
-    # cv2.waitKey()
-    return out
+    def getframeByTime(self,time):
+        """
+        指定时间节点读取任意帧
+        """
+        out, err = (
+            ffmpeg.input(self.inFile, ss=time)
+                  .output('pipe:', vframes=1, format='image2', vcodec='mjpeg')
+                  .run(capture_stdout=True)
+        )
+        return out
 
 
-def get_video_info(in_file):
-    """
-    获取视频基本信息
-    """
-    try:
-        probe = ffmpeg.probe(in_file)
+    def getVideoInfo(self):
+        """
+        获取视频基本信息
+        """
+        probe = ffmpeg.probe(self.inFile)
         video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
         if video_stream is None:
             print('No video stream found', file=sys.stderr)
             sys.exit(1)
         return video_stream
-    except ffmpeg.Error as err:
-        print(str(err.stderr, encoding='utf8'))
-        sys.exit(1)
+
 
 if __name__ == '__main__':
     file_path = 'C:/Users/xiaoli/Desktop/VID20230702033914.mp4'
     video_info = get_video_info(file_path)
+    width = int(video_stream['width'])
+    height = int(video_stream['height'])
     total_duration = video_info['duration']
     print('总时间：' + total_duration + 's')
     random_time = random.randint(1, int(float(total_duration)) - 1) + random.random()
@@ -50,7 +47,6 @@ if __name__ == '__main__':
     print(type(out))
     image_array = numpy.asarray(bytearray(out), dtype="uint8")
     print(type(image_array))
+    image_array = numpy.asarray(bytearray(out), dtype="uint8")
     image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-    cv2.imshow('frame', image)
-    cv2.imwrite('C:/Users/xiaoli/Desktop/test2.jpg',image)
-    cv2.waitKey()
+    cv2.inweite(self.outFile, image)
