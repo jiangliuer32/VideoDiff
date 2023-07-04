a1adacca85347ac98eb0a2742c3c67b2c7a95ecc
2952902226aA <132554136+2952902226aA@users.noreply.github.com>
2023-07-04 19:13:04 +0800

    debug

---
+++ Run.py
@@ -0,0 +1,4 @@
+i = 1
+outFile = "/Users/v_liyujiang/Desktop"
+outFile = outFile + "/" + str(i) + ".img"
+print(outFile)
--- VideoDiff.py
+++ VideoDiff.py
@@ -1,16 +1,36 @@
 from VideoInfo import VideoInfo
+import numpy
 import math
+import cv2
+
+
 class VideoDiff():
-    def __init__(self,inFileA,inFileB,duration):
+    def __init__(self,inFileA,inFileB,outFile,duration):
         self.inFileA = inFileA
         self.inFileB = inFileB
+        self.outFile = outFile
         self.duration = duration
 
     def videoDiff(self):
         videoA = VideoInfo(self.inFileA)
-        videoB = VideoInfo(self.inFileB)
         infoA  = videoA.getVideoInfo()
+
+        videoB = VideoInfo(self.inFileB)
         infoB  = videoB.getVideoInfo()
 
-        if infoB['duration'] ==
+        if math.floor(infoB['duration']) != math.floor(infoA['duration']):
+            print("视频时长不相等")
+        else:
+            vDuration = math.floor(infoB['duration'])%self.duration
+
+            for i in (1,vDuration+1):
+                time = time + vDuration
+                out = videoA.getframeByTime(time)
+
+                image_array = numpy.asarray(bytearray(out), dtype="uint8")
+                image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+                outFile = outFile + "\\" + str(i) + ".img"
+                cv2.inweite(self.outFile, image)
+
+
 
